6857623b9be7a9654ee82dc18e2663f45fbfd6c0
Priyanshu <work.priyanshu085@gmail.com>
2025-12-14 23:08:41 +0530

    Update to Next v16.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,23 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const eslintConfig = [{
+  ignores: ["**/generated/*"],
+}, ...nextCoreWebVitals, ...nextTypescript, ...compat.config({
+  plugins: ["@typescript-eslint"],
 
-const eslintConfig = [
-  {
-    ignores: ["**/generated/*"],
-  },
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    plugins: ["@typescript-eslint"],
-    rules: {
-      "no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/no-explicit-any": ["warn"],
-      "@typescript-eslint/no-non-null-asserted-optional-chain": ["warn"],
-      "@typescript-eslint/no-unused-expressions": ["warn"],
-    },
-  }),
-];
+  rules: {
+    "no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "@typescript-eslint/no-explicit-any": ["warn"],
+    "@typescript-eslint/no-non-null-asserted-optional-chain": ["warn"],
+    "@typescript-eslint/no-unused-expressions": ["warn"],
+  }
+})];
 
 export default eslintConfig;
--- pages/_error.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ErrorPage() {
-  return <div>An error occurred</div>;
-}
